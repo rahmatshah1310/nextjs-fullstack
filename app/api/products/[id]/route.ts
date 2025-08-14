2c1714186e9f7ee060dc641e46776d4cd129267c
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-15 03:19:34 +0500

    add

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,36 +1,28 @@
 import { prisma } from "@/db.server";
+import { NextRequest } from "next/server";
 
-// Get Single Product
-export async function GET(req: Request, context: { params: { id: string } }) {
-  const { id } = context.params;
-
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+  const { id } = params;
   const product = await prisma.product.findUnique({
     where: { id: Number(id) },
   });
-
   return new Response(JSON.stringify(product), { status: 200 });
 }
 
-// Update a Single Product
-export async function PUT(req: Request, context: { params: { id: string } }) {
-  const { id } = context.params;
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+  const { id } = params;
   const { name, description, price } = await req.json();
-
   const product = await prisma.product.update({
     where: { id: Number(id) },
     data: { name, description, price: parseFloat(price) },
   });
-
   return new Response(JSON.stringify(product), { status: 200 });
 }
 
-// Delete a Single Product
-export async function DELETE(req: Request, context: { params: { id: string } }) {
-  const { id } = context.params;
-
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+  const { id } = params;
   await prisma.product.delete({
     where: { id: Number(id) },
   });
-
   return new Response(JSON.stringify({ success: true }), { status: 200 });
 }
