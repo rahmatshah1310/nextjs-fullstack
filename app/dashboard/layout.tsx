04752019acb94ec40c2aebebac8b98719a332013
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-16 01:09:48 +0500

    add

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,14 @@
-// app/dashboard/layout.tsx (Server Component)
+// app/dashboard/layout.tsx
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import AppLayout from "../layouts/AppLayout";
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-  const user = await getServerSession();
+  const session = await getServerSession();
 
-  if (!user) {
+  if (!session) {
     redirect("/auth/login");
   }
 
-  return <AppLayout session={user}>{children}</AppLayout>;
+  return <AppLayout session={session}>{children}</AppLayout>;
 }
