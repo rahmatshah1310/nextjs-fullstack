3f324c586e0f3553eab2d81086521262621898e4
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-19 22:35:26 +0500

    add

--- components/common/Model.tsx
+++ components/common/Model.tsx
@@ -9,18 +9,19 @@ interface ModalProps {
   onClose: (open: boolean) => void;
   children: React.ReactNode;
   description?: string;
+  className?: string;
 }
 
-export function Modal({ title, description, open, onClose, children }: ModalProps) {
+export function Modal({ title, description, open, onClose, children, className }: ModalProps) {
   return (
     <Dialog open={open} onOpenChange={onClose}>
       <DialogContent
-        className="sm:max-w-lg 
+        className={`sm:max-w-sm
     bg-white/70 dark:bg-gray-900/70   
     backdrop-blur-xl                
     border border-white/20           
     shadow-2xl                      
-    rounded-2xl"
+    rounded-2xl ${className}`}
       >
         <DialogHeader>
           <DialogTitle className="text-center">{title}</DialogTitle>
